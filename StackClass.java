3162d79a413dde5992007a408da97eb82e4cc986
Saroj Budhathoki <budhathokisaroj827@gmail.com>
2024-03-14 11:20:11 +0545

    using java collection farame works

--- StackClass.java
+++ StackClass.java
@@ -1,62 +1,48 @@
+import java.util.ArrayList;
+
 public class StackClass {
     
-    static class Node{
-        int data;
-        Node next;
+    static class Stack {
+        static ArrayList<Integer> list = new ArrayList<>();
 
-        public Node(int data){
-            this.data = data;
-            this.next = null;
+        public boolean isEmpty() {
+            return list.isEmpty();
         }
-    }
 
-     static class Stack{
+        public void push(int data) {
+            list.add(data);
+        }
 
-        public static Node head;
-        public static boolean isEmpty(){
-            return head == null;
-        }
-        public static void push (int data){
-            Node newNode = new Node(data);
-            if(head == null){
-                head = newNode;
-                return;
-            }
-            newNode.next = head;
-            head = newNode;
-
-        }
-        public static void pop(){
-            if(head == null){
-                System.out.println("Stack is empty");
-                return;
-            }
-            head = head.next;
-        }
-        public static int peek(){
-            if(head == null){
-                System.out.println("Stack is empty");
+        public int pop() {
+            if (isEmpty()) {
                 return -1;
             }
-            return head.data;
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
         }
+        
 
-     }
-public static void main(String[] args) {
-
-    Stack s = new Stack();
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-    s.push(50);
-    
-    while(!s.isEmpty()){
-        System.out.println(s.peek());
-        s.pop();
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
     }
 
-    
-}
+    public static void main(String[] args) {
 
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }

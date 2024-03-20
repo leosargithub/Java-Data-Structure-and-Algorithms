953a69fbee09b5d571442f2745808854cc864067
Saroj Budhathoki <budhathokisaroj827@gmail.com>
2024-03-20 20:27:17 +0545

    add preorder traversal

--- BinaryTreesYT.java
+++ BinaryTreesYT.java
@@ -32,6 +32,16 @@ public class BinaryTreesYT {
             }
         }
 
+        public  static void preorder(Node root){
+            if(root == null){
+                return;
+            }
+
+            System.out.println(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
     public static void main(String[] args) {
         
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
@@ -40,6 +50,7 @@ public class BinaryTreesYT {
         Node root = tree.buildTree(nodes);
 
         System.out.println(root.data);
+        preorder(root);
 
     }
     

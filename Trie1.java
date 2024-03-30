a2e7a67e2409d4f82dc099f95f4240c330b199ac
Saroj Budhathoki <budhathokisaroj827@gmail.com>
2024-03-30 08:30:36 +0545

    to add element in tier data structure

---
+++ Trie1.java
@@ -0,0 +1,62 @@
+public class Trie1 {
+
+    static class Node{
+
+        Node[] children ;
+        boolean eow;
+
+        public Node(){
+         children = new Node[26];
+         for(int i=0; i<26; i++){
+                children[i] = null;
+         }
+            eow = false;
+        }
+    }
+ static Node root = new Node();
+
+ public static void insert(String word){
+    Node curr = root; 
+
+    for(int i=0; i<word.length(); i++){
+        int index = word.charAt(i) - 'a';
+        if(curr.children[index] == null){
+            curr.children[index] = new Node();
+        }
+        if(i == word.length()-1){
+            curr.children[index].eow = true;
+        }
+
+        curr = curr.children[index];
+    }
+ }
+ public static boolean search(String key){
+    Node curr = root;
+
+    for(int i=0; i<key.length(); i++){
+        int index = key.charAt(i) - 'a';
+        Node node = curr.children[index];
+        if(node == null){
+            return false;
+        }
+        if(i == key.length()-1 && node.eow == false){
+            return false;
+        }
+        curr = curr.children[index];
+    }
+    return true;
+}
+    public static void main(String[] args) {
+        
+        String words[] = {"apple", "app", "apart", "bat", "ball", "cat", "dog", "doggy", "dogged"};
+
+        for(int i=0; i<words.length; i++){
+            insert(words[i]);
+        }
+        System.out.println(search("apple"));
+        System.out.println(search("app"));
+        System.out.println(search("apart"));
+        System.out.println(search("car"));
+
+    }
+}
